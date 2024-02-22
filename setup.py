7c46fae5b46056105bdae9d9c45a2d2450e42ea8
zxw2600 <zhaoxinwei74@gmail.com>
2024-02-22 15:41:16 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apriltag_calibrate',
+    version='1.0.0',
+    description='Apriltag Bundle Calibrate Tools',
+    author='ZXW2600',
+    author_email='zhaoxinwei74@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ]
+)
